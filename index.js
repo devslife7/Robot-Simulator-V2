6d9a26df05b40c7f9731c87c9e7775046e832d00
Marcos Velasco <devslife7@gmail.com>
2020-07-15 22:48:36 -0400

    done

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", () => {
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  const moveUl = document.getElementById('moves-container')
+  const moveButton = document.getElementById('move-button')
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key == "ArrowRight" || e.key == "ArrowLeft" || e.key == "ArrowUp" || e.key == "ArrowDown"){
+      const keyPress = e.key.slice(5).toLowerCase()
+      
+      const moveLi = document.createElement('li')
+      moveLi.textContent = keyPress
+      moveUl.append(moveLi)
+    }
+
+    if (e.key == "Delete"){
+      moveUl.removeChild(moveUl.lastChild)
+    }
+  })
+
+  moveButton.addEventListener('click', (r) => {
+    const firstMoveText = moveUl.firstChild.textContent.toLowerCase()
+    move(firstMoveText)
+
+    moveUl.firstChild.remove()
+  })
 })
